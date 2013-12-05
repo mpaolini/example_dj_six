3dd2e90eb27b95e834d4c25bc04b7409246f7b6c
Journeyman <jman@storiepvtride.it>
2013-12-05 13:41:36 +0100

    fixes: string formatting (python 2.7+), delete method

--- dj_six/perf/client.py
+++ dj_six/perf/client.py
@@ -10,32 +10,31 @@ from .models import Item
 
 def get_item(url, key, session=None):
     if session is None:
-        r = requests.get("http://%s/%s/%d/" % (url, "item", key))
+        r = requests.get("http://{}/{}/{}/".format(url, "item", key))
     else:
-        r = session.get("http://%s/%s/%d/" % (url, "item", key))
+        r = session.get("http://{}/{}/{}/".format(url, "item", key))
     return r.text
 
 def get_items(url, session=None):
     if session is None:
-        r = requests.get("http://%s/%s/" % (url, "item"))
+        r = requests.get("http://{}/{}/".format(url, "item"))
     else:
-        r = session.get("http://%s/%s/" % (url, "item"))
-
+        r = session.get("http://{}/{}/".format(url, "item"))
     return r.text
 
 def post_item(url, key, value, session=None):
     payload = { "key": key, "value": value }
     if session is None:
-        r = requests.post("http://%s/%s/" % (url, "item"), data=payload)
+        requests.post("http://{}/{}/".format(url, "item"), data=payload)
     else:
-        r = session.post("http://%s/%s/" % (url, "item"), data=payload)
+        session.post("http://{}/{}/".format(url, "item"), data=payload)
 
 def delete_items(url, session=None):
     j = get_items(url)
     objs = json.loads(j)
     for item in objs:
-        _url = "http://%s/%s/%s/" % (url, "item", item['key'])
+        _url = "http://{}/{}/{}/".format(url, "item", item['key'])
         if session is None:
-            r = requests.post(_url, headers={'X-HTTP-Method-Override': 'DELETE'})
+            requests.delete(_url)
         else:
-            r = session.post(_url, headers={'X-HTTP-Method-Override': 'DELETE'})
+            session.delete(_url)
