3b76365e4502c6a8ba9cb0e4e7e58ad6e09b8e60
Marco Paolini <marco@credra.com>
2013-12-07 11:08:52 +0100

    Make django settings more modular.

--- dj_six/settings/common.py
+++ dj_six/settings/common.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$w25=%+nq03c=we@*fb7w@4tqjptmkhpqxe&q5u&g64qg&@d-*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '$w25=%+nq03c=we@*fb7w@4tqjptmkhpqxe&q5u&g64qg&@d-*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
